0f7cf16144cf90a2bfb6ab1ecd087daf368b30e9
Fabricio <fabri_0077@hotmail.com>
2022-11-16 22:48:16 -0300

    se modifica la ruta de aperturapuerta

--- routes/aperturaPuerta_route.js
+++ routes/aperturaPuerta_route.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express.Router();
 const aperturaPuertaControler = require("../controler/aperturaPuerta_controler");
 
-app.get("/insert/:id/:status/", async function(req,res){
+app.get("/:id/:status/", async function(req,res){
     const data = {
         id : req.params.id,
         status : req.params.status
@@ -15,7 +15,7 @@ app.get("/insert/:id/:status/", async function(req,res){
 
 
 
-app.get("/status/:id/", async function(req,res){
+app.get("/:id/", async function(req,res){
     const id =  req.params.id;
     const entrada = await aperturaPuertaControler.statusPuerta(id);
     res.json(entrada);
