604904962e5b86e07c8264108bf6ff3165539d16
iva <ivapruzzo@gmail.com>
2023-05-20 20:55:21 -0300

    Se actualiza la consulta a la base de datos para agregar en real time la parte de sacar el ultimo dato de temperatura y ppm

--- model/realTime.js
+++ model/realTime.js
@@ -5,7 +5,7 @@ module.exports = {
     devolverDato: async function(id){
       try {
         
-            const query = 'SELECT aperturapuerta.status_puerta,luz.status_luz,movimiento.status_movimiento,aperturaventana.status_ventana FROM aperturapuerta JOIN movimiento ON aperturapuerta.id = movimiento.id AND aperturapuerta.id = ? JOIN luz ON aperturapuerta.id = luz.id AND aperturapuerta.id = ? JOIN aperturaventana ON aperturapuerta.id = aperturaventana.id';  
+            const query = 'SELECT aperturapuerta.status_puerta,luz.status_luz,movimiento.status_movimiento,aperturaventana.status_ventana,temperatura.temp,monoxido.ppm FROM aperturapuerta JOIN movimiento ON aperturapuerta.id = movimiento.id AND aperturapuerta.id = 1 JOIN luz ON aperturapuerta.id = luz.id AND aperturapuerta.id = 1 JOIN aperturaventana ON aperturapuerta.id = aperturaventana.id JOIN temperatura ON aperturapuerta.id = temperatura.id JOIN monoxido ON aperturapuerta.id = monoxido.id ORDER by temperatura.register DESC, monoxido.register DESC LIMIT 1';  
            
             const respuesta = await conexion.query(query, [id,id]);
             
